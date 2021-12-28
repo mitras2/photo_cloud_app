5743541b670f86a3c9a6aa38301ac8dda6d82c0e
Luc <mitras2@web.de>
2021-12-28 01:51:00 +0100

    Improved the handling of uris and the uri paths

--- lib/api/api_paths.dart
+++ lib/api/api_paths.dart
@@ -6,10 +6,16 @@ const String _apiCreateUser = _apiBaseUser;
 const String _apiDeleteUser = _apiBaseUser;
 const String _apiGetUser = '$_apiBaseUser/';
 
-
-
-
 Uri userLoginUri({required Uri serverUri}) {
-  String targetPath = serverUri.path + _apiUserLogin;
+  String basePath = pathWithoutTrailingSlash(uriPath: serverUri.path);
+  String targetPath = basePath + _apiUserLogin;
   return serverUri.replace(path: targetPath);
+}
+
+String pathWithoutTrailingSlash({required String uriPath}) {
+  var path = uriPath;
+  while (path.endsWith("/")) {
+    path = path.substring(0, path.length -1);
+  }
+  return path;
 }
